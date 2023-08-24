fe86d53ecd7c8d207f1a378d65108c4d3cbe7b9e
William Chatfield <william.chatfield@cardinalhealth.com>
2023-08-24 10:06:12 -0400

    [LRCQ-8652] Nio & functional implementation

--- src/Main.java
+++ src/Main.java
@@ -1,32 +1,28 @@
 
 import java.io.*;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.stream.Stream;
 
 public class Main {
-    public static final File HOME = new File(System.getProperty("user.home"));
-    public static final File INPUT_FILE_DIR = new File(HOME,"Downloads\\SORS-2023");
-    public static final File OUTPUT_FILE = new File(HOME,"Downloads\\SORS-2023.txt");
-
-    public static File[] getFilesInDirectory(File dir) throws IOException {
-        File[] files = dir.listFiles();
-        if (files == null) {
-            throw new IOException("Not a directory or I/O error: " + dir);
-        }
-        return files;
-    }
+    public static final Path HOME = Path.of(System.getProperty("user.home"));
+    public static final Path INPUT_FILE_DIR = HOME.resolve("Downloads\\SORS-2023");
+    public static final Path OUTPUT_FILE = HOME.resolve("Downloads\\SORS-2023.txt");
 
     public static void main(String[] args) {
         try {
-            try (PrintWriter out = new PrintWriter(new FileWriter(OUTPUT_FILE))) {
-                File[] inputFiles = getFilesInDirectory(INPUT_FILE_DIR);
-                for (File inputFile : inputFiles) {
-                    if (inputFile.isFile() && !inputFile.getName().contains("NOEVENTS")) {
-                        try (BufferedReader reader = new BufferedReader(new FileReader(inputFile))) {
-                            String line;
-                            while ((line = reader.readLine()) != null) {
-                                String trimmedLine = line.trim();
-                                if (!trimmedLine.contains("*") && !trimmedLine.contains("SORS")) {
-                                    out.println(trimmedLine);
-                                }
+            try (PrintWriter out = new PrintWriter(Files.newBufferedWriter(OUTPUT_FILE))) {
+                try (DirectoryStream<Path> inputFiles = Files.newDirectoryStream(INPUT_FILE_DIR)) {
+                    for (Path inputFile : inputFiles) {
+                        if (Files.isRegularFile(inputFile) && !inputFile.toString().contains("NOEVENTS")) {
+                            try (Stream<String> lines = Files.lines(inputFile)) {
+                                lines.forEach(line -> {
+                                    String trimmedLine = line.trim();
+                                    if (!trimmedLine.contains("*") && !trimmedLine.contains("SORS")) {
+                                        out.println(trimmedLine);
+                                    }
+                                });
                             }
                         }
                     }
